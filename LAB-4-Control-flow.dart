9f78082f10f60767a2fb1617f3ac9876dd981637
Daniel <Danielasratab@gmail.com>
2024-03-08 16:25:13 +0300

    LAB-4-Control-flow.dart

---
+++ LAB-4-Control-flow.dart
@@ -0,0 +1,35 @@
+// Exercise 4
+import 'dart:io';
+
+void main() {
+  print("Enter the first number:");
+  double num1 = double.parse(stdin.readLineSync()!);
+
+  print("Enter the second number:");
+  double num2 = double.parse(stdin.readLineSync()!);
+
+  print("Enter the operation (+, -, *, /):");
+  String operation = stdin.readLineSync()!;
+
+  double result = 0;
+
+  switch (operation) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    case '/':
+      result = num1 / num2;
+      break;
+    default:
+      print("Invalid operation.");
+      return;
+  }
+
+  print("Result: $result");
+}
